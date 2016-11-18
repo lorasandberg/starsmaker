3742fd84ff91c418b339638d2463865d3c223293
Saryos <ear@cs.tut.fi>
2016-11-19 00:03:58 +0200

    userinterface grabbing works even better

--- Assets/UserInterface.cs
+++ Assets/UserInterface.cs
@@ -3,25 +3,49 @@ using System.Collections;
 
 public class UserInterface : MonoBehaviour {
 
+	bool mousehold = false;
+	Rigidbody heldBody = null;
 	// Use this for initialization
 	void Start () {
 	
 	}
-
-	void OnMouseDown ()
-	{
-		Debug.Log("hit");
-		Debug.Log( Input.mousePosition);
-	}
+		
 	// Update is called once per frame
 	void Update () {
+
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 
-		Debug.Log("hop");
-		if (Physics.Raycast(ray, out hit, 1000)){
-			Debug.DrawLine(ray.origin, hit.point);
-			Debug.Log( hit.point);
+		//nothing happens if we are out of game plane
+		if (Physics.Raycast (ray, out hit, 1000)) {
+
+			if (Input.GetMouseButton (0)) {
+				// we are not helding an object
+				if (!mousehold) {
+					if (hit.rigidbody) {
+						//  existing object
+						Debug.Log("catch");
+						mousehold = true;
+						heldBody = hit.rigidbody;
+					} else{
+						Debug.Log ("no object");
+						// create new body
+					}
+				} else {
+					// we are holding an object and applying velocity change
+					Vector3 rubberBand = hit.point - heldBody.position;
+					// limit maximum acceleration with minimum mass
+					heldBody.velocity += (rubberBand / Mathf.Max (heldBody.mass, 100)) * Time.deltaTime;
+					Debug.Log ("rubber");
+
+				}
+			}
+
+		} else {
+			// no mouse button 0, release object
+			heldBody = null;
 		}
+
+
 	}
 }
