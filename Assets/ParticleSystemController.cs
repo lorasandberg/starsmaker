4a06f600586d88bb68653f2de7134509b43bbb0c
Antti Sandberg <antti.sandberg@gmail.com>
2016-11-18 22:25:32 +0200

    Added controller scripts

---
+++ Assets/ParticleSystemController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSystemController : MonoBehaviour {
+    public ParticleSystem particleSystem;
+    ParticleSystem.Particle[] particles;
+
+    // Use this for initialization
+    void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        particles = new ParticleSystem.Particle[particleSystem.maxParticles];
+        int aliveParticles = particleSystem.GetParticles(particles);
+
+        for (int i = 0; i < particles.Length; i++)
+        {
+            Vector3 v = particles[i].position;
+            v.z = 0;
+            particles[i].position = v;
+        }
+
+        particleSystem.SetParticles(particles, aliveParticles);
+	}
+}
---
+++ Assets/StellarObjectController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StellarObjectController : MonoBehaviour {
+
+    public GameObject prefab;
+    List<GameObject> stellarObjects;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start");
+        stellarObjects = new List<GameObject>();
+        GameObject asd = (GameObject)Object.Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        stellarObjects.Add(asd);
+
+        Rigidbody rb = asd.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
