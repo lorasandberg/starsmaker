04fada0bce727105504c78cd4c264ba048f0da5a
Antti Sandberg <antti.sandberg@gmail.com>
2016-11-18 23:55:43 +0200

    Spiral

---
+++ Assets/CenterController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CenterController : MonoBehaviour {
+    public GameObject center;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        center.transform.Rotate(0, 0.3f, 0);
+	}
+}
--- Assets/ParticleSystemController.cs
+++ Assets/ParticleSystemController.cs
@@ -19,10 +19,34 @@ public class ParticleSystemController : MonoBehaviour {
         for (int i = 0; i < particles.Length; i++)
         {
             Vector3 v = particles[i].position;
-            v.z = 0;
-            particles[i].position = v;
+
+            Vector2 p = new Vector2(particles[i].position.x, particles[i].position.y);
+
+            Vector2 angled = new Vector2(p.x, p.y);
+            float distanceFromCenter = p.magnitude;
+            angled = Rotate(angled, 10f / p.magnitude * 0.003f);
+
+            Vector2 newP = Vector2.MoveTowards(p, angled, 100);
+
+            particles[i].position = new Vector3(newP.x, newP.y, 0);
+
+
         }
 
         particleSystem.SetParticles(particles, aliveParticles);
 	}
+
+    Vector2 Rotate(Vector2 v, float angle)
+    {
+        Vector2 vector = new Vector2(v.x, v.y);
+        float sin = Mathf.Sin(angle);
+        float cos = Mathf.Cos(angle);
+
+        float tx = vector.x;
+        float tz = vector.y;
+        vector.x = (cos * tx) + (sin * tz);
+        vector.y = (cos * tz) - (sin * tx);
+        
+        return vector;
+    }
 }
