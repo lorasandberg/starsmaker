2d82718aa97dd5f55832af26f203a085f79bbdeb
Antti Sandberg <antti.sandberg@gmail.com>
2016-11-20 03:05:31 +0200

    Bug fixes

---
+++ Assets/KeepAtPlace.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepAtPlace : MonoBehaviour {
+    Vector3 initPos;
+	// Use this for initialization
+	void Start () {
+        initPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = initPos;
+	}
+}
