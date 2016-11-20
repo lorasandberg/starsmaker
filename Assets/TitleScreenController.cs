7b53e886b3b16e9f127884f2fdc87baf77ed810f
Antti Sandberg <antti.sandberg@gmail.com>
2016-11-20 15:27:18 +0200

    Title screen

--- Assets/TitleScreenController.cs
+++ Assets/TitleScreenController.cs
@@ -6,14 +6,12 @@ using System.Collections;
 public class TitleScreenController : MonoBehaviour {
 
     public Button startButton;
-    public Button instructionsButton;
     public Button quitButton;
 
     // Use this for initialization
 	void Start ()
     {
         startButton.GetComponent<Button>().onClick.AddListener(StartGame);
-        instructionsButton.GetComponent<Button>().onClick.AddListener(OpenInstructions);
         quitButton.GetComponent<Button>().onClick.AddListener(QuitGame);
     }
 	
